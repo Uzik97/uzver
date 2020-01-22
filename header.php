0b2957373ad20883a4a8258fb1db5acfb75f9ee4
Uzik97 <bmwvladosa777@gmail.com>
2020-01-22 15:19:16 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+      <title>Carousel Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="libs/jqueryui/jquery-ui.css">
+    <link rel="stylesheet" href="libs/colorbox/colorbox.css">
+    <link rel="stylesheet" href="libs/superfish/css/superfish.css">
+    <link rel="stylesheet" href="libs/superfish/css/superfish-vertical.css">
+    <link rel="stylesheet" href="libs/superfish/css/superfish-navbar.css">
+    <link rel="stylesheet" href="libs/superfish/css/megafish.css">
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<!-- End WOWSlider.com HEAD section -->
+    <link rel="stylesheet" href="libs/bxslider/jquery.bxslider.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/example.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="horizon scroll">
+        <img src="images/horizon.png" alt="" />
+      </div>
+      <div class="middle scroll">
+         <img src="images/middle.png" alt="" />
+      </div>
+      <div class="front scroll">
+        <img src="images/front.png" alt="" />
+      </div>
+    </header>
